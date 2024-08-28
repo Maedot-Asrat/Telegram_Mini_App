5f17cce8d44c52bccdff4ebc8c3dc52629f5c17c
Maedoot Asrat <asratmaedot@gmail.com>
2024-08-29 02:03:10 +0300

    Maddy Hamster

--- src/main.tsx
+++ src/main.tsx
@@ -4,22 +4,20 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import TaskPage from './TaskPage';
-import { PointsProvider } from './PointsContext'
+import { PointsProvider } from './PointsContext';
 
 const Main: React.FC = () => {
-  const [points, setPoints] = React.useState(0);
-
-  const updatePoints = (newPoints: number) => {
-    setPoints(newPoints);
+  const updatePoints = () => {
+    // You can still handle point updates here if needed
   };
 
   return (
     <Router>
       <PointsProvider>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/task" element={<TaskPage updatePoints={updatePoints} />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/task" element={<TaskPage updatePoints={updatePoints} />} />
+        </Routes>
       </PointsProvider>
     </Router>
   );
