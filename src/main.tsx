7529d3d27dd64ff591ad9b3aecde8ac9d2a80d15
Maedoot Asrat <asratmaedot@gmail.com>
2024-09-05 00:30:22 +0300

    updated

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import TaskPage from './TaskPage';
+import TaskPageProps from './TaskPage';
 import { PointsProvider } from './PointsContext';
 
 const Main: React.FC = () => {
@@ -20,7 +20,7 @@ const Main: React.FC = () => {
         <Routes>
           <Route path="/" element={<Navigate to="/api/user/:token" />} />
           <Route path="/api/user/:token" element={<App />} />
-          <Route path="/task" element={<TaskPage updatePoints={updatePoints} />} />
+          <Route path="/task" element={<TaskPageProps updatePoints={updatePoints} />} />
         </Routes>
       </PointsProvider>
     </Router>
